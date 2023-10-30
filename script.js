34a317915b6a0284f773a590dab640ec370c39b3
Allison Farris <allison.perry98@gmail.com>
2023-10-29 18:44:06 -0600

    added task function to add time to tasks added

--- script.js
+++ script.js
@@ -15,7 +15,40 @@ $('saveBtn').on('click', function () {
   localStorage.setItem(time, text);
 });
 
-$(function () {
+
+// $('#hour8 .description').val(localStorage.getItem('hour8'));
+// $('#hour9 .description').val(localStorage.getItem('hour9'));
+// $('#hour10 .description').val(localStorage.getItem('hour10'));
+// $('#hour11 .description').val(localStorage.getItem('hour11'));
+// $('#hour12 .description').val(localStorage.getItem('hour12'));
+// $('#hour13 .description').val(localStorage.getItem('hour13'));
+// $('#hour14 .description').val(localStorage.getItem('hour14'));
+// $('#hour15 .description').val(localStorage.getItem('hour15'));
+// $('#hour16 .description').val(localStorage.getItem('hour16'));
+// $('#hour17 .description').val(localStorage.getItem('hour17'));
+
+$(function tasks() {
+
+  var currentHour = today.hours();
+  $('time-block').each(function () {
+    var timeId = parseInt($(this).attr('id').split('hour')[1]);
+
+    if (timeId < currentHour) {
+      $(this).addClass('past');
+    }
+    else if (timeId === currentHour) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    }
+    else {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    }
+  });
+
+  tasks();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
