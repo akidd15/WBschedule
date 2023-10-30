7decdaa4c173d6c1ed3d232aee870f8158f1fea4
Allison Farris <allison.perry98@gmail.com>
2023-10-29 18:33:37 -0600

    added variables and local storage

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var today = moment();
+
+var timeBlockEL = document.querySelector('.container');
+
+
+$('#currentDay').text(today.format('LLLL'));
+
+$('saveBtn').on('click', function () {
+  var text = $(this).siblings('.description').val();
+  var time = $(this).parent().attr('id');
+
+  localStorage.setItem(time, text);
+});
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
